7e2d0f5ed31cc5b5cfb1985366da05731f1956da
li671207 <635245976@qq.com>
2020-05-01 18:58:51 +0800

    启动时检查配置  check

--- user-service-consumer/src/main/java/com/lihl/gmall/ConsumerMain.java
+++ user-service-consumer/src/main/java/com/lihl/gmall/ConsumerMain.java
@@ -9,8 +9,9 @@ public class ConsumerMain {
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
 		
-		OrderService orderService = context.getBean(OrderService.class);
-		orderService.initOrder("1");
+		//测试启动时检查
+//		OrderService orderService = context.getBean(OrderService.class);
+//		orderService.initOrder("1");
 	
 		System.in.read();
 	}
