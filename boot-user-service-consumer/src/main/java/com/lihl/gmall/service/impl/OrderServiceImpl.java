3dfd38137b9f3a75db38a84d1898174026f2baa1
li671207 <635245976@qq.com>
2020-05-02 19:02:07 +0800

    dubbo 负载均衡，默认随机

--- boot-user-service-consumer/src/main/java/com/lihl/gmall/service/impl/OrderServiceImpl.java
+++ boot-user-service-consumer/src/main/java/com/lihl/gmall/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.rpc.cluster.loadbalance.RoundRobinLoadBalance;
 
 import com.lihl.gmall.bean.UserAddress;
 import com.lihl.gmall.service.OrderService;
@@ -13,7 +14,8 @@ import com.lihl.gmall.service.UserService;
 @Service
 public class OrderServiceImpl implements OrderService {
 	
-	@Reference(url = "127.0.0.1:20881")//绕过注册中心,dubbo直连方式
+//	@Reference(url = "127.0.0.1:20881")//绕过注册中心,dubbo直连方式
+	@Reference(loadbalance = RoundRobinLoadBalance.NAME)//负载均衡，默认随机
 	private UserService userService;
 
 	public List<UserAddress> initOrder(String userId) {
--- boot-user-service-provider/src/main/java/com/lihl/gmall/service/impl/UserServiceImpl.java
+++ boot-user-service-provider/src/main/java/com/lihl/gmall/service/impl/UserServiceImpl.java
@@ -13,6 +13,7 @@ import com.lihl.gmall.service.UserService;
 public class UserServiceImpl implements UserService {
 
 	public List<UserAddress> getUserAddressList(String userId) {
+		System.out.println("UserServiceImpl......3...");
 		UserAddress userAddress1 = new UserAddress(1,"广东深圳","001","lihl","13888888888","Y");
 		UserAddress userAddress2 = new UserAddress(1,"四川成都","002","jww","13999999999","Y");
 		
